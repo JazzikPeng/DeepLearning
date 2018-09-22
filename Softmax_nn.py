d174a91f7a07a4d36520299a35ec56aca3ff1346
Jazzik Peng <ZHEJIANPENG@YAHOO.COM>
2018-09-21 22:42:09 -0500

    Update and rename HW1.py to Softmax_nn.py

--- Softmax_nn.py
+++ Softmax_nn.py
@@ -1,12 +1,4 @@
 
-# coding: utf-8
-
-# # HW1
-# Implement and train a neural network from scratch in Python for the MNIST dataset (no PyTorch). The neural network should be trained on the Training Set using stochastic gradient descent. It should achieve 97-98% accuracy on the Test Set. For full credit, submit via Compass (1) the code and (2) a paragraph (in a PDF document) which states the Test Accuracy and briefly describes the implementation. Due September 7 at 5:00 PM.
-
-# In[1]:
-
-
 import numpy as np
 import h5py
 import time
