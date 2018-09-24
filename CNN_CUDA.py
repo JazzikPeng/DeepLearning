537b613d5b374af9749e558cb6f8fc2042f8cc52
Jazzik Peng <ZHEJIANPENG@YAHOO.COM>
2018-09-23 21:00:59 -0500

    Update CNN_CUDA.py

--- CNN_CUDA.py
+++ CNN_CUDA.py
@@ -3,14 +3,8 @@
 
 # Created on Sept. 19, 2018
 '''
-HW3: Train a deep convolution network on a GPU with PyTorch for the CIFAR10 dataset.
-The convolution network should use (A) dropout, (B) trained with RMSprop or ADAM, and (C)
-data augmentation. For 10% extra credit, compare dropout test accuracy (i) using the heuristic
-prediction rule and (ii) Monte Carlo simulation. For full credit, the model should achieve 80-90%
-Test Accuracy. Submit via Compass (1) the code and (2) a paragraph (in a PDF document) which
-reports the results and briefly
-describes the model architecture.
-Due September 28 at 5:00 PM.
+CIFAR10 dataset.
+This convolution network should used dropout, trained with ADAM, and data augmentation. 
 '''
 import numpy as np
 import torch
